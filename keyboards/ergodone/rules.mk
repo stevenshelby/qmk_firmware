d6f27245a4af9ceb5babc876bf96bfc4cab6682b
Steven Shelby <stevenshelby@gmail.com>
2019-08-09 11:24:11 -0400

    update config for shelbs

--- keyboards/ergodone/rules.mk
+++ keyboards/ergodone/rules.mk
@@ -73,12 +73,12 @@ OPT_DEFS += -DBOOTLOADER_SIZE=4096
 CUSTOM_MATRIX           = yes # Custom matrix file for the ErgoDone
 UNICODE_ENABLE          = yes # Unicode
 BOOTMAGIC_ENABLE        = yes	# Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE         = yes	# Mouse keys(+4700)
-EXTRAKEY_ENABLE         = yes	# Audio control and System control(+450)
+MOUSEKEY_ENABLE         = no	# Mouse keys(+4700)
+EXTRAKEY_ENABLE         = no	# Audio control and System control(+450)
 CONSOLE_ENABLE          = no	# Console for debug(+400)
 COMMAND_ENABLE          = no  # Commands for debug and configuration
 SLEEP_LED_ENABLE        = no  # Breathing sleep LED during USB suspend
-NKRO_ENABLE             = yes	# USB Nkey Rollover - not yet supported in LUFA
+NKRO_ENABLE             = no	# USB Nkey Rollover - not yet supported in LUFA
 USB_6KRO_ENABLE         = no  # USB 6key Rollover
 BACKLIGHT_ENABLE        = no	# Enable keyboard backlight functionality
 KEYMAP_IN_EEPROM_ENABLE = no 	# External keymap in eeprom
